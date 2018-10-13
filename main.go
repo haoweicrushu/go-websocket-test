0daed58f563990e823751eea89ea3d2d14cf34e7
haowei <haoweicrushu@gmail.com>
2018-10-13 21:47:00 +0800

    web socket practice

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func (r *http.Request) bool {
+		return true
+	},
+}
+
+func chatroom(c *gin.Context) {
+
+	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close()
+	for {
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		if string(message) == "foo" {
+			message = []byte("bar")
+		}
+
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			break
+		}
+	}
+}
+
 func main() {
-	
+	bindAddress := "localhost:8888"
+	r := gin.Default()
+	r.GET("/chatroom", chatroom)
+	r.Run(bindAddress)
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 # go web socket test
+respect
+https://hacpai.com/article/1524469717858
+
+1. install dep
+2. dep ensure
+3. go run main.go
+4. open index.html console
